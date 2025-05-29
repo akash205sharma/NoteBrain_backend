cc92c8c1ccf5ad09fefc9ce0c799115beeed2cd8
Akash2005sharma <akashsharma.cpp@gmail.com>
2025-05-29 10:51:35 +0530

    env setup

--- src/index.js
+++ src/index.js
@@ -2,23 +2,27 @@ import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 import runRoute from './routes/run.js';
-import setupSocket from './socket.js';
+// import setupSocket from './socket.js';
 import cors from 'cors';
+// require("dotenv").config();
+import { configDotenv } from 'dotenv';
+
+configDotenv();
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer, {
-  cors: { origin: '*' }
-});
+// const io = new Server(httpServer, {
+//   cors: { origin: '*' }
+// });
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/run', runRoute);
 
-setupSocket(io);  // Socket.IO collaboration logic
+// setupSocket(io);  // Socket.IO collaboration logic
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3001;
 httpServer.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
